bb77dc113017cb143b1db6d628b7b60acc02a8c4
tom555my <tom555my>
2023-07-19 08:36:13 +0000

    Update title

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ function App(props) {
             align="center"
             className={classes.title}
           >
-            EKS DEMO Blog
+            ABCD DEF
           </Typography>
           {new Date().toLocaleTimeString()}
         </Toolbar>
